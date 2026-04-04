368f0d689b833080d005c558bc002b5025f22d1b
RenzoJimenezB <adrianrjb@outlook.es>
2026-04-03 19:44:31 -0500

    chore(migration): seed default admin user

---
+++ src/main/resources/db/migration/V4__seed_admin_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (email, password, role)
+VALUES (
+        'admin@inspectpro.com',
+        '$2b$12$2GqBt9PEPITiKlgU6JAUT.os4v729qPCWwJRdS.5LBQMYrF5i8dpW',
+        'ADMIN'
+       );
