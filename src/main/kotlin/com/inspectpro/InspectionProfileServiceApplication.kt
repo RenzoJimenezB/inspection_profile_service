4b44b7213cbc966b9255bdd5a1f2618cdbc0dc0f
RenzoJimenezB <adrianrjb@outlook.es>
2026-04-05 16:11:08 -0500

    chore(config): enable scheduling

--- src/main/kotlin/com/inspectpro/InspectionProfileServiceApplication.kt
+++ src/main/kotlin/com/inspectpro/InspectionProfileServiceApplication.kt
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(JwtProperties::class)
+@EnableScheduling
 class InspectionProfileServiceApplication
 
 fun main(args: Array<String>) {
